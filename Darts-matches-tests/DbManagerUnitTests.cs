234c994e52e5e04a0eaf93fca64c52884ecbd2a1
bencher <bencher@users.noreply.github.com>
2021-10-01 15:05:42 +0200

    connect to database
    connection to database succesful
    should work on all computers during development

---
+++ Darts-matches-tests/DbManagerUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Darts_matches;
+
+namespace Darts_matches_tests
+{
+    [TestClass]
+    public class DbManagerUnitTests
+    {
+        [TestMethod]
+        public void TestConnection()
+        {
+            //arrange
+            Darts_matches.DbManager dbManager = Darts_matches.DbManager.getInstance();
+            string expected = "Open";
+
+            //act
+            dbManager.addToDatabase();
+
+            //assert
+            string state = dbManager.getState();
+            Assert.AreEqual(expected, state, "connection not established: " + state);
+        }
+    }
+}
--- Darts-matches/DbManager.cs
+++ Darts-matches/DbManager.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
+using System.IO;
 using System.Diagnostics;
 
 namespace Darts_matches
 {
-    class DbManager
+    public class DbManager
     {
+        private string state;
+        public string getState() { return state; }
+
         private DbManager() { }
 
         private static DbManager _instance;
@@ -20,9 +25,19 @@ namespace Darts_matches
             return _instance;
         }
 
+        private void connect()
+        {
+            string projectDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
+            using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='" + projectDir.ToString() + @"\Darts-matches\DatabaseMatches.mdf';Integrated Security=True"))
+            {
+                connection.Open();
+                state = connection.State.ToString();
+            }
+        }
+
         public void addToDatabase()
         {
-
+            connect();
         }
 
         public void pullFromDatabase()
