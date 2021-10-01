f3dc6496804983fcae23469207ab6dcb1e6a8df7
bencher <bencher@users.noreply.github.com>
2021-10-01 12:19:54 +0200

    dbManager class added
    dbManager class added with singleton pattern

---
+++ Darts-matches/DbManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Darts_matches
+{
+    class DbManager
+    {
+        private DbManager() { }
+
+        private static DbManager _instance;
+
+        public static DbManager getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new DbManager();
+            }
+            return _instance;
+        }
+
+        public void addToDatabase()
+        {
+
+        }
+
+        public void pullFromDatabase()
+        {
+            
+        }
+    }
+}
