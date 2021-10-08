f9770c8f92b868c2bc3a56dd6661efb17ddcd03c
bencher <bencher@users.noreply.github.com>
2021-10-08 12:50:03 +0200

    naming conventions
    alle variabele en functie namen geupdate naar de code conventions

--- Darts-matches-tests/DatabaseControllerUnitTests.cs
+++ Darts-matches-tests/DatabaseControllerUnitTests.cs
@@ -4,20 +4,20 @@ using Darts_matches;
 namespace Darts_matches_tests
 {
     [TestClass]
-    public class DbManagerUnitTests
+    public class DatabaseControllerUnitTests
     {
         [TestMethod]
         public void TestConnectionOpen()
         {
             //arrange
-            Darts_matches.DbManager dbManager = Darts_matches.DbManager.getInstance();
+            Darts_matches.Controllers.DatabaseController dbManager = Darts_matches.Controllers.DatabaseController.GetInstance();
             string expected = "Open";
 
             //act
-            dbManager.testConnection();
+            dbManager.TestConnection();
 
             //assert
-            string state = dbManager.getState1();
+            string state = dbManager.GetState1();
             Assert.AreEqual(expected, state, "connection not established: " + state);
         }
 
@@ -25,14 +25,14 @@ namespace Darts_matches_tests
         public void TestConnectionClose()
         {
             //arrange
-            Darts_matches.DbManager dbManager = Darts_matches.DbManager.getInstance();
+            Darts_matches.Controllers.DatabaseController dbManager = Darts_matches.Controllers.DatabaseController.GetInstance();
             string expected = "Closed";
 
             //act
-            dbManager.testConnection();
+            dbManager.TestConnection();
 
             //assert
-            string state = dbManager.getState2();
+            string state = dbManager.GetState2();
             Assert.AreEqual(expected, state, "connection not established: " + state);
         }
 
@@ -40,14 +40,14 @@ namespace Darts_matches_tests
         public void TestDatabaseInsert()
         {
             //arrange
-            Darts_matches.DbManager dbManager = Darts_matches.DbManager.getInstance();
+            Darts_matches.Controllers.DatabaseController dbManager = Darts_matches.Controllers.DatabaseController.GetInstance();
             bool expected = true;
 
             //act
-            dbManager.addToDatabase("dutch-open", "test", 501, "tester1", "tester1", "tester2", "1:2:2:1:2", "2:3:1:3:0", "2:3:1:3:0", 23, 43, "130:70:100", "130:70:100", "105:101:80;90:80", "105:101:80;90:80", 5, 1, System.DateTime.Today);
+            dbManager.AddToDatabase("dutch-open", "test", 501, "tester1", "tester1", "tester2", "1:2:2:1:2", "2:3:1:3:0", "2:3:1:3:0", 23, 43, "130:70:100", "130:70:100", "105:101:80;90:80", "105:101:80;90:80", 5, 1, System.DateTime.Today);
 
             //assert
-            bool succes = dbManager.getSucces();
+            bool succes = dbManager.GetSucces();
             Assert.AreEqual(expected, succes, "data not inserted into database");
         }
     }
