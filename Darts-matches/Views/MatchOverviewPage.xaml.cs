09423f11312e4a7588d435705a400ba67b5d1d52
bencher <bencher@users.noreply.github.com>
2021-10-15 14:17:54 +0200

    zoekscherm laat juiste data zien
    de data die niet in het zoekscherm moet staan is nu eruit gehaald
    de overige data is ook geordend

--- Darts-matches/Views/MatchOverviewPage.xaml.cs
+++ Darts-matches/Views/MatchOverviewPage.xaml.cs
@@ -18,6 +18,29 @@ namespace Darts_matches
             DatabaseController dbc = DatabaseController.GetInstance();
             DataTable dt = dbc.PullAllFromDatabase();
             dg_overview.ItemsSource = dt.AsDataView();
+
+            Loaded += MatchOverviewPage_loaded;
+        }
+
+        private void MatchOverviewPage_loaded(object sender, RoutedEventArgs e)
+        {
+            dg_overview.Columns[0].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[3].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[4].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[7].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[8].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[9].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[10].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[11].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[12].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[13].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[14].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[15].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[16].Visibility = Visibility.Collapsed;
+            dg_overview.Columns[17].Visibility = Visibility.Collapsed;
+
+            dg_overview.Columns[2].DisplayIndex = 17;
+
         }
 
         private void btn_select_match_Click(object sender, System.Windows.RoutedEventArgs e)
