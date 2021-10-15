270bf14bae294ccd09010981cc07589df95e2f1a
G1020BomberSquad <johan01.meer@gmail.com>
2021-10-15 12:51:05 +0200

    can now select a match and show all info

--- Darts-matches/Views/MatchOverviewPage.xaml.cs
+++ Darts-matches/Views/MatchOverviewPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using Darts_matches.Controllers;
-using Darts_matches.Views;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Darts_matches
@@ -17,6 +18,21 @@ namespace Darts_matches
             DatabaseController dbc = DatabaseController.GetInstance();
             DataTable dt = dbc.PullAllFromDatabase();
             dg_overview.ItemsSource = dt.AsDataView();
+        }
+
+        private void btn_select_match_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DataRowView selectedRow = (DataRowView)dg_overview.SelectedItems[0];
+
+            object[] rowItemArray = selectedRow.Row.ItemArray;
+
+            string showRow = "";
+            foreach (var item in rowItemArray)
+            {
+                showRow += item.ToString() + "\n";
+            }
+
+            MessageBox.Show(showRow);
         }
     }
 }
