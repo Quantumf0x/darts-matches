9da87896e55a6a2c2e2ec7a9c19ff20f1185a435
G1020BomberSquad <johan01.meer@gmail.com>
2021-10-29 15:51:25 +0200

    Removed creation of dummy data

--- Darts-matches/Controllers/DatabaseController.cs
+++ Darts-matches/Controllers/DatabaseController.cs
@@ -183,15 +183,6 @@ namespace Darts_matches.Controllers
             }
         }
 
-        private void FillDatabaseDummy()
-        {
-            Trace.WriteLine("Adding to database");
-            AddToDatabase("Test match name", "niks", 501, "player1", "player1", "player2", "2:1:1:2:2", "2:3:3:1:1", "3:1:2:3:3", 50, 50, "130:70:50:10:30", "130:70:50:10:30", "105:101:80;80:30:50;40:60:50", "105:101:80;80:30:50;40:60:50", 4, 8, new DateTime(2021, 10, 20, 20, 30, 45));
-            //AddToDatabase("Test match name", "niks", 501, "player1", "player1", "player2", "2:1:1:2:2", "2:3:3:1:1", "3:1:2:3:3", 50, 50, "130:70:50:10:30", "130:70:50:10:30", "105:101:80;80:30:50;40:60:50", "105:101:80;80:30:50;40:60:50", 4, 8, new DateTime(2021, 10, 21, 12, 44, 45));
-            //AddToDatabase("Test match name", "niks", 501, "player1", "player1", "player2", "2:1:1:2:2", "2:3:3:1:1", "3:1:2:3:3", 50, 50, "130:70:50:10:30", "130:70:50:10:30", "105:101:80;80:30:50;40:60:50", "105:101:80;80:30:50;40:60:50", 4, 8, new DateTime(2021, 10, 22, 15, 30, 45));
-            //AddToDatabase("Test match name", "niks", 501, "player1", "player1", "player2", "2:1:1:2:2", "2:3:3:1:1", "3:1:2:3:3", 50, 50, "130:70:50:10:30", "130:70:50:10:30", "105:101:80;80:30:50;40:60:50", "105:101:80;80:30:50;40:60:50", 4, 8, new DateTime(2021, 10, 23, 20, 17, 45));
-        }
-
         private void Connect()
         {
             if (_connection == null)
@@ -249,7 +240,6 @@ namespace Darts_matches.Controllers
 
         public DataTable PullAllFromDatabase()
         {
-            FillDatabaseDummy();
             Connect();
             SqlCommand command = _connection.CreateCommand();
             command.CommandText = "SELECT * FROM dbo.matches;";
