a752b362c42085a95e8d9054fccf4119b4bc00dd
G1020BomberSquad <johan01.meer@gmail.com>
2021-10-29 11:42:02 +0200

    data

--- Darts-matches/Controllers/DatabaseController.cs
+++ Darts-matches/Controllers/DatabaseController.cs
@@ -45,7 +45,7 @@ namespace Darts_matches.Controllers
         {
             Trace.WriteLine(Environment.CurrentDirectory);
             string databasePath = Environment.CurrentDirectory + "\\DatabaseMatches.mdf";
-            string databaseLogPath = Environment.CurrentDirectory + "\\DatabaseMatchesLog.ldf";
+            string databaseLogPath = Environment.CurrentDirectory + "\\DatabaseMatches_log.ldf";
 
             // search for exsisting database
             if (File.Exists(databasePath))
@@ -55,6 +55,8 @@ namespace Darts_matches.Controllers
             else
             {
                 SetupDatabase(databasePath, databaseLogPath);
+                _connection = null;
+                SetConnection();
             }
 
         }
@@ -62,7 +64,7 @@ namespace Darts_matches.Controllers
         private void SetupDatabase(string databasePath, string databaseLogPath)
         {
             // create database
-            _connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;Integrated security=True;database=master");
+            _connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;Integrated security=True;");
             string createDatabaseString =
                 "DROP DATABASE DatabaseMatches;" +
                 "CREATE DATABASE DatabaseMatches ON PRIMARY " +
@@ -95,7 +97,9 @@ namespace Darts_matches.Controllers
             }
             finally
             {
+                Trace.WriteLine(_connection.Database);
                 _connection.ChangeDatabase("DatabaseMatches");
+                Trace.WriteLine(_connection.Database);
                 SetupDatabaseTables();
             }
         }
@@ -117,7 +121,7 @@ namespace Darts_matches.Controllers
                 legs_won_per_set_player2 TEXT NOT NULL,
                 average_per_3_darts_total_player1 INT NOT NULL,
                 average_per_3_darts_total_player2 INT NOT NULL,
-                verage_per_3_darts_per_set_player1 TEXT NOT NULL,
+                average_per_3_darts_per_set_player1 TEXT NOT NULL,
                 average_per_3_darts_per_set_player2 TEXT NOT NULL,
                 average_per_3_darts_per_leg_player1 TEXT NOT NULL,
                 average_per_3_darts_per_leg_player2 TEXT NOT NULL,
@@ -145,7 +149,7 @@ namespace Darts_matches.Controllers
                 {
                     ClearTables();
                     _connection.Close();
-                    //FillDatabaseDummy();
+                    _connection = null;
                 }
             }
         }
@@ -175,7 +179,6 @@ namespace Darts_matches.Controllers
             AddToDatabase("Test match name", "niks", 501, "player1", "player1", "player2", "2:1:1:2:2", "2:3:3:1:1", "3:1:2:3:3", 50, 50, "130:70:50:10:30", "130:70:50:10:30", "105:101:80;80:30:50;40:60:50", "105:101:80;80:30:50;40:60:50", 4, 8, new DateTime(2021, 10, 21, 12, 44, 45));
             AddToDatabase("Test match name", "niks", 501, "player1", "player1", "player2", "2:1:1:2:2", "2:3:3:1:1", "3:1:2:3:3", 50, 50, "130:70:50:10:30", "130:70:50:10:30", "105:101:80;80:30:50;40:60:50", "105:101:80;80:30:50;40:60:50", 4, 8, new DateTime(2021, 10, 22, 15, 30, 45));
             AddToDatabase("Test match name", "niks", 501, "player1", "player1", "player2", "2:1:1:2:2", "2:3:3:1:1", "3:1:2:3:3", 50, 50, "130:70:50:10:30", "130:70:50:10:30", "105:101:80;80:30:50;40:60:50", "105:101:80;80:30:50;40:60:50", 4, 8, new DateTime(2021, 10, 23, 20, 17, 45));
-
         }
 
         private void Connect()
@@ -211,13 +214,21 @@ namespace Darts_matches.Controllers
                 " '" + average_per_3_darts_total_player2 + "', '" + average_per_3_darts_per_set_player1 + "', '" + average_per_3_darts_per_set_player2 + "'," +
                 " '" + average_per_3_darts_per_leg_player1 + "', '" + average_per_3_darts_per_leg_player2 + "', '" + number_180_player1 + "'," +
                 " '" + number_180_player2 + "', '" + date.Year + date.Month + date.Day + "')";
-            if (command.ExecuteNonQuery() == 1)
+
+            try
             {
-                _succes = true;
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    _succes = true;
+                }
+                else
+                {
+                    _succes = false;
+                }
             }
-            else
+            catch (Exception err)
             {
-                _succes = false;
+                Trace.WriteLine("ERR: " + err);
             }
 
             Close();
@@ -227,6 +238,7 @@ namespace Darts_matches.Controllers
 
         public DataTable PullAllFromDatabase()
         {
+            FillDatabaseDummy();
             Connect();
             SqlCommand command = _connection.CreateCommand();
             command.CommandText = "SELECT * FROM dbo.matches;";
